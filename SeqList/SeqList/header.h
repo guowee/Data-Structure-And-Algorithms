4ff045c771d2b3fc5278aeea43a6dd87e9f58a4b
guowee <guoweegw@gmail.com>
2019-01-05 01:48:57 +0800

    线性表的顺序表示和实现

--- SeqList/SeqList/header.h
+++ SeqList/SeqList/header.h
@@ -4,6 +4,9 @@
 #define OVERFLOW -1
 #define OK 1
 #define ERROR 0
+#define INFEASIBLE -2
+#define TRUE 1
+#define FALSE 0
 typedef int Status;
 typedef int ElemType;
 /*
@@ -17,8 +20,15 @@ typedef struct
 }SqList;
 
 Status InitList(SqList &L);
-Status InsertElement(SqList &L, int index,ElemType e);
-Status DeleteElement(SqList &L, int index,ElemType &e);
-Status TraverseList(SqList L,void(*vi)(ElemType*));
-
+Status DestoryList(SqList &L);
+Status ClearList(SqList &L);
+Status ListEmpty(SqList L);
+int ListLength(SqList L);
+Status GetElem(SqList L, int index, ElemType &e);
+int LocateElem(SqList L, ElemType e, Status(*compare)(ElemType, ElemType));
+Status PriorElem(SqList L, ElemType cur_e, ElemType &pre_e);
+Status NextElem(SqList L, ElemType cur_e, ElemType &next_e);
+Status ListInsert(SqList &L, int index,ElemType e);
+Status ListDelete(SqList &L, int index,ElemType &e);
+Status ListTraverse(SqList L,void(*vi)(ElemType*));
 #endif
--- SeqList/SeqList/main.cpp
+++ SeqList/SeqList/main.cpp
@@ -1,17 +1,78 @@
 #include <stdio.h>
 #include "header.h"
 
+Status equal(ElemType c1, ElemType c2)
+{ /* 判断是否相等的函数，Union()用到 */
+	if (c1 == c2)
+		return TRUE;
+	else
+		return FALSE;
+}
+
 void print(ElemType *c)
 {
 	printf("%d ", *c);
 }
-void main() {
 
-	SqList list;
-	InitList(list);
+void Union(SqList &La,SqList Lb) {
+ /*将所有在线性表Lb中但不在La中的数据元素插入到La中*/
+	ElemType e;
+	int La_len, Lb_len;
+	int i;
+	La_len = ListLength(La);
+	Lb_len = ListLength(Lb);
+	for (i = 1; i <= Lb_len;i++) {
+		GetElem(Lb, i, e);
+		if (!LocateElem(La, e, equal)) {
+			ListInsert(La,++La_len,e);
+		}
+	}
+}
+
+
+void MergeList(SqList La,SqList Lb,SqList &Lc) {
+	/*已知线性表La和Lb中的数据元素按值非递减排列，归并La和Lb得到新的线性表Lc,Lc中的数据元素也按非递减排列*/
+	InitList(Lc);
+	ElemType ai, bj;
+	int La_len, Lb_len;
+	int i =1, j = 1, k = 0;
+	La_len = ListLength(La);
+	Lb_len = ListLength(Lb);
+	while ((i <= La_len) && (j <= Lb_len)) {
+		GetElem(La, i, ai);
+		GetElem(Lb, j, bj);
+		if (ai<=bj) {
+			ListInsert(Lc,++k,ai);
+			++i;
+		}
+		else {
+			ListInsert(Lc, ++k, bj);
+			++j;
+		}
+	}
+
+	while (i <= La_len) {
+		GetElem(La, i++, ai);
+		ListInsert(Lc, ++k, ai);
+	}
+
+	while (j <= Lb_len)
+	{
+		GetElem(Lb, j++, bj);
+		ListInsert(Lc, ++k, bj);
+	}
+}
+
+
+
+
+int main() {
+
+	SqList La,Lb;
+	InitList(La);
 	for (int i = 0; i < 10; i++)
 	{
-		Status status = InsertElement(list,i+1,i+5);
+		Status status = ListInsert(La,i+1,i+5);
 		if (status == OK) {
 			printf("Insert Element Success! \n");
 		}
@@ -20,28 +81,29 @@ void main() {
 		}
 	}
 
-	TraverseList(list,print);
+	ListTraverse(La,print);
 
 	ElemType e;
-	DeleteElement(list, 2, e);
+	ListDelete(La, 2, e);
 	print(&e);
 	printf("\n");
-	TraverseList(list, print);
+	ListTraverse(La, print);
+
+	InitList(Lb);
+	int j;
+	for (j = 1; j <= 5; j++)
+	{
+		ListInsert(Lb, j, 4 * j);
+	}
+	ListTraverse(Lb,print);
+
+	SqList Lc;
+	MergeList(La, Lb, Lc);
+	ListTraverse(Lc, print);
+
+	Union(La, Lb);
+
+	ListTraverse(La,print);
+
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
